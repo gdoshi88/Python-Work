41cc0f22cd0a3d8d630d79624b8273ceba22ab5f
Gautam Doshi <Pariah@gautams-mbp.attlocal.net>
2019-06-10 13:30:15 -0700

    python homework

---
+++ PyBank/PyBank Script/main.py
@@ -0,0 +1,133 @@
+import os, csv
+
+
+#access and read csv file
+budgetdata_csv = os.path.join("..", "budget_data.csv")
+
+date_list = []
+profit_loss_list = []
+sumprofit = 0
+profit = 0
+profit_loss = 0
+
+
+
+
+
+with open(budgetdata_csv, newline="", encoding="UTF8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    csv_header = next(csv_reader, None)
+
+    #print(csv_header)
+
+    for row in csv_reader:
+        date_list.append(row[0])
+        profit_loss_list.append(row[1])
+
+        
+    
+    # sum of profit and loss
+        profit = int(row[1])
+        if profit > 0: 
+            sumprofit = profit + sumprofit
+        elif profit < 0:
+            profit_loss = profit + profit_loss
+
+        totalprofitloss = sumprofit + profit_loss
+
+#define the revenue change for average
+greatest_profit = profit_loss_list[0]
+greatest_loss = profit_loss_list[0]
+tot_revenue_for_change = 0
+
+for r in range(len(profit_loss_list)):
+    if profit_loss_list[r] >= greatest_profit:
+        greatest_profit = profit_loss_list[r]
+        greatest_profit_month = date_list[r]
+
+    elif profit_loss_list[r] <= greatest_loss:
+        greatest_loss = profit_loss_list[r]
+        greatest_loss_month = date_list[r]
+
+# print(greatest_loss)
+# print(greatest_profit)
+    
+
+
+        
+
+        
+
+
+
+       
+
+        # if int(greatest_profit[1]) < int(profit_loss_list[1])
+
+
+
+        
+
+       
+       
+       
+        
+
+
+
+
+    
+
+        # print(date)
+        # print(profit_loss)
+
+
+
+    
+
+
+print("Financial Analysis")
+print("-" * 20)
+print("Total Months:",len(date_list))
+print("Total: $", totalprofitloss)
+print("Average Change: $" + Average)
+
+
+
+# print(sumprofit) 
+# print(totalprofitloss)
+
+
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PyPoll/PyPoll Script/main.py
@@ -0,0 +1,48 @@
+import os
+import csv
+
+tot_votes = 0
+candidate = ""
+candidate_votes = {}
+candidate_percent = {}
+winner_votes = 0
+winner = ""
+
+filepath = os.path.join("..", "election_data.csv")
+
+with open(filepath,'r', newline="", encoding="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csvheader = next(csvreader, None)
+
+
+    # votes
+    for row in csvreader:
+        tot_votes = tot_votes + 1
+        candidate = row[2]
+        if candidate in candidate_votes:
+            candidate_votes[candidate] = candidate_votes[candidate] + 1
+        else:
+            candidate_votes[candidate] = 1
+
+
+
+# voter pecent and winner
+for person, vote_count in candidate_votes.items():
+    candidate_percent[person] = '{0:.0%}'.format(vote_count / tot_votes)
+    if vote_count > winner_votes:
+        winner_votes = vote_count
+        winner = person
+
+dash = ("-" * 20)
+
+# print out results
+print("Election Results")
+print(dash)
+print(f"Total Votes: {tot_votes}")
+print(dash)
+for person, vote_count in candidate_votes.items():
+    print(f"{person}: {candidate_percent[person]} ({vote_count})")
+print(dash)
+print(f"Winner: {winner}")
+print(dash)
